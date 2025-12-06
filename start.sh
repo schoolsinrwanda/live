71f511adf43dbb2981c03314b5e1965b955e133d
mvoice <97416245+schoolsinrwanda@users.noreply.github.com>
2025-12-06 13:56:16 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,11 +2,11 @@
 python3 server.py &
 
 while true; do
-    ffmpeg -re -stream_loop -1 -i strea.mp4 \
--c:v libx264 -preset veryfast -g 120 -keyint_min 120 \
--b:v 1000k -maxrate 1000k -bufsize 2000k \
+ ffmpeg -re -stream_loop -1 -i streamm.mp4 \
+-c:v libx264 -preset superfast -g 60 -keyint_min 60 \
+-b:v 800k -maxrate 800k -bufsize 1600k \
 -vf "scale=720:1280:flags=lanczos" \
 -pix_fmt yuv420p -profile:v high -level 4.0 \
 -c:a aac -ar 44100 -b:a 128k -ac 2 \
--f flv rtmp://a.rtmp.youtube.com/live2/c35y-6fmx-crep-buu7-7w1h
+-f flv rtmp://a.rtmp.youtube.com/live2/mmgs-zdwg-mj6p-7ajh-arq2
 done
